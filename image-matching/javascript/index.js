2cc65b37dfe70317b552d5eeb8ec728e3be22080
Oluwatobi Immanuel <agbeleyeoluwatobi@gmail.com>
2023-06-26 14:12:23 +0100

    made changes

--- image-matching/javascript/index.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-let countMatches = (grid1, grid2) => {
-    //:Todo implement this method based on the specification on the readme.md
-};
-
-module.exports = countMatches;
--- image-matching/php/index.php
+++
@@ -1,6 +0,0 @@
-<?php
-	function countMatches($grid1, $grid2) {
-		//:Todo implement this method based on the specification on the readme.md
-		return -1;
-	}
-?>
